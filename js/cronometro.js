107b9f8c92d9f81625b3056023a3274167a750cb
Yesica Perlaza <perlazayesik@gmail.com>
2022-11-15 20:13:10 -0500

    empezando codigo cronometro.js

---
+++ js/cronometro.js
@@ -0,0 +1,19 @@
+(function(){
+    "use strict"
+
+    const d = document;
+    const crH = d.querySelector(".crH");
+    const crM = d.querySelector(".crM");
+    const crS = d.querySelector(".crS");
+
+    d.addEventListener("click",(e)=>{
+
+        if(e.target.matches(".btnCrReset")){
+
+        }
+
+        if(e.target.matches(".btnCrIniciar")){
+
+        }
+    })
+})();
