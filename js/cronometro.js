e7d0768c6329a8380501bde0980927b43cf8d4ce
Yesica Perlaza <perlazayesik@gmail.com>
2022-11-16 18:23:12 -0500

    terminando de configurar cronometro.js

--- js/cronometro.js
+++ js/cronometro.js
@@ -5,15 +5,52 @@
     const crH = d.querySelector(".crH");
     const crM = d.querySelector(".crM");
     const crS = d.querySelector(".crS");
+    let a,b,c;
+
+
+    const iniciarCronometro = ()=>{
+        let hora = 0;
+        let min = 0;
+        let seg = 0;
+
+        a = setInterval(()=>{
+            seg++;
+
+            if(seg < 10) crS.innerHTML = `0${seg}`;
+            if(seg >= 10)crS.innerHTML = `${seg}`;
+            if(seg > 59) crS.innerHTML =`00`;
+            if(seg === 60) seg = 0;
+            
+        },1000);
+
+        b = setInterval(()=>{
+            min++;
+
+            if(min < 10) crM.innerHTML = `0${min}:`;
+            if(min >= 10)crM.innerHTML = `${min}:`;
+            if(min > 59) crM.innerHTML =`00:`;
+            if(min === 60) min = 0;
+
+        },60000);
+
+        c =setInterval(()=>{
+            hora++;
+
+            if(hora < 10) crH.innerHTML = `0${hora}:`;
+            if(hora >= 10)crH.innerHTML = `${hora}:`;
+            if(hora > 23) crH.innerHTML =`00:`;
+            if(hora === 24) hora = 0;
+
+        },3600000);
+
+    }
 
     d.addEventListener("click",(e)=>{
 
         if(e.target.matches(".btnCrReset")){
-
+            clearInterval(c);
         }
 
-        if(e.target.matches(".btnCrIniciar")){
-
-        }
+        if(e.target.matches(".btnCrIniciar"))iniciarCronometro();
     })
 })();
